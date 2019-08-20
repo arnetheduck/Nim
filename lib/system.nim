a49fb90df20c40e7ed7e80bc919d027cb78b72b9
Jacek Sieka <arnetheduck@gmail.com>
2019-08-20 08:22:30 +0200

    add nlvm system import used for nlvm runtime support

--- lib/system.nim
+++ lib/system.nim
@@ -2326,6 +2326,9 @@ when notJSnotNims:
   when defined(memtracker):
     include "system/memtracker"
 
+  when defined(nlvm):
+    import nlvm_system
+
   when hostOS == "standalone":
     include "system/embedded"
   else:
