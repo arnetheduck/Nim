664ca43b07933026d8becce5b9c315100c7d2a8c
Jacek Sieka <arnetheduck@gmail.com>
2022-10-29 14:37:09 +0200

    fix c_memchr, c_strstr definitions

--- lib/pure/strutils.nim
+++ lib/pure/strutils.nim
@@ -1849,8 +1849,8 @@ func find*(a: SkipTable, s, sub: string, start: Natural = 0, last = 0): int {.
   return -1
 
 when not (defined(js) or defined(nimdoc) or defined(nimscript)):
-  func c_memchr(cstr: pointer, c: char, n: csize_t): pointer {.
-                importc: "memchr", header: "<string.h>".}
+  from system/ansi_c import c_memchr
+
   const hasCStringBuiltin = true
 else:
   const hasCStringBuiltin = false
@@ -1875,7 +1875,7 @@ func find*(s: string, sub: char, start: Natural = 0, last = 0): int {.rtl,
     when hasCStringBuiltin:
       let L = last-start+1
       if L > 0:
-        let found = c_memchr(s[start].unsafeAddr, sub, cast[csize_t](L))
+        let found = c_memchr(s[start].unsafeAddr, cint(sub), cast[csize_t](L))
         if not found.isNil:
           return cast[ByteAddress](found) -% cast[ByteAddress](s.cstring)
     else:
